527618b584b2e77ccc045a82362318932688ad9b
santiago <san010180@gmail.com>
2017-10-26 22:13:24 -0500

    Primera versión

--- archivo Javascript.js
+++ archivo Javascript.js
@@ -1,6 +1,13 @@
 /**
  * Created by peextssolis on 26/10/2017.
  */
-function crearUsuario() {
-    return "usuario creado";
-}
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h1>Bienvenidos a Node js</h1>');
+    res.write('<p>Clase front end.</p>');
+    res.end('');
+}).listen(8080);
+console.log('El servidor se encuentra ejecutandose.' )
+
